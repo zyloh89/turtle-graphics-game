100f6c63dbfd4500ca3501180869cac05e382462
Ziyen <zyloh89@gmail.com>
2020-08-15 11:51:45 +0930

    add border and food

--- kbgame1.py
+++ kbgame1.py
@@ -1,18 +1,41 @@
 # Turtle Graphics Game – Space Turtle Chomp
 
 import turtle
+import math
+import random
 
 # Set up screen
 turtle.setup(650,650)
 wn = turtle.Screen()
-wn.bgcolor('navy')
+wn.bgcolor('midnightblue')
+
+# Draw border
+mypen = turtle.Turtle()
+mypen.penup()
+mypen.setposition(-300, -300)
+mypen.pendown()
+mypen.pensize(2)
+mypen.color('lightsteelblue')
+for side in range(4):
+    mypen.forward(600)
+    mypen.left(90)
+mypen.hideturtle()
 
 # Create player turtle
 player = turtle.Turtle()
-player.color('thistle')
+player.color('hotpink1')
 player.shape('turtle')
 player.penup() #won't leave a line when it moves, eg. penup you don't draw, pendown you do draw
 player.speed(0)
+
+# Create food
+food = turtle.Turtle()
+food.color("seagreen3")
+food.shape("circle")
+food.penup()
+food.speed(0)
+food.setposition(-100, 100)
+food.setposition(random.randint(-290, 290), random.randint(-290, 290))
 
 # Set speed variable
 speed = 1
@@ -37,3 +60,17 @@ turtle.onkey(increase_speed, 'Up')
 
 while True:
     player.forward(speed)
+
+    # Boundary Player Checking x coordinate
+    if player.xcor() >290 or player.xcor() <-290:
+        player.right(180)
+    
+    #Boundary Player Checking y coordinate
+    if player.ycor() >290 or player.ycor() <-290:
+        player.right(180)
+
+    # Collision checking
+    d = math.sqrt(math.pow(player.xcor() - food.xcor(), 2) + math.pow(player.ycor() - food.ycor(),2))
+    if d < 20:
+        food.hideturtle()
+
